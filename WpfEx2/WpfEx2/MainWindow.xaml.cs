ab7aadb5b781474d5d5330b9b8173798641df0c7
sarah <sarahhan.one@gmail.com>
2025-07-01 18:10:42 +0900

    day 01

---
+++ WpfEx2/WpfEx2/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfEx2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Product> products = new List<Product>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            prodlist.ItemsSource = products;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Int32.Parse(idv.Text);
+            string name = namev.Text;
+            int price = Int32.Parse(pricev.Text);
+            int amount = Int32.Parse(amountv.Text);
+            bool isout = false;
+            if (isoutv.IsChecked != null) {
+                isout = (bool)isoutv.IsChecked;
+            }
+            products.Add(new Product(id, name, price, amount, isout));
+            prodlist.Items.Refresh();//항목이 변경됐으므로 뷰 리프레시
+            idv.Text = "";
+            namev.Text = "";
+            pricev.Text = "";
+            amountv.Text = "";
+            isoutv.IsChecked = false;
+
+        }
+    }
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Amount { get; set; }
+        public bool isOut { get; set; }
+        public Product() { }
+        public Product(int id, string name, int price, int amount, bool isout)
+        {
+            Id = id;
+            Name = name; 
+            Price = price;
+            Amount = amount;
+            isOut = isout;
+        }
+    }
+}
---
+++ wpfEx1/wpfEx1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfEx1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> list = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            list.Add("aaa");
+            list.Add("bbb");
+            list.Add("ccc");
+            lst1.ItemsSource = list; //ui의 리스트박스와 list를 바인딩
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("click button");
+            txt.Text = input.Text;
+        }
+
+        //sender:이벤트 발생 객체(클릭된 라디오버튼)
+        private void ra1_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton ra = (RadioButton)sender;
+            MessageBox.Show(ra.Content+"");
+            
+        }
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return "[id="+Id+", name:"+Name+"]";
+        }
+    }
+}
